8b8234f020978a99a8a08567c2c262a3585e9fa6
Xiaoli Jiang <jiangxiaoli1104@gmail.com>
2015-05-26 14:25:24 -0700

    deploy to heroku

---
+++ project/Build.scala
@@ -0,0 +1,15 @@
+//dependencies management
+
+import sbt._
+import Keys._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "My first application"
+  val appVersion      = "1.0"
+
+  val appDependencies = Seq(
+    "postgresql" % "postgresql" % "8.4-702.jdbc4"
+  )
+
+}
